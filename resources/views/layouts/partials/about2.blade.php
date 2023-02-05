a2a7be13fb9e894f197c5f1340c6765783a3db6a
Herman Ceaser <ceaserbanks09@gmail.com>
2023-02-05 18:06:45 +0300

    added new photos

--- resources/views/layouts/partials/about2.blade.php
+++ resources/views/layouts/partials/about2.blade.php
@@ -10,21 +10,22 @@
                         <div class="sub-title"><span class="icon"></span> We believe what we achieve</div>
                         <h2>Honesty & Respect For <br>Giving Human Resources</h2>
                     </div>
-                    <div class="lower-box">
-                        <div class="image">
-                            <img src="{{ Vite::asset('resources/images/resource/video-image-2.jpg') }}" alt="">
-                            <a href="https://www.youtube.com/watch?v=C9rVAbcQH_U" class="lightbox-image">
-                                <span class="fa fa-play"></span>
-                            </a>
-                        </div>
-                        <div class="text">Consectetur adipisicing elit usmod tempor incididunt laboret dolore magna aliquaut enim mini veniam quis trud exercitation ullamco laboris nisiut aliquip ex ea consequat. Duis aute rue dolor prehendrit lorem psum dolor sit amet consectetur adipisicing.</div>
+                    <div class="lower-box pl-0">
+                        Aid widow and Orphans Education support Organization (AWOESO) is a
+                        christian relief and development Non- Government Organization with it’s head offices in
+                        Juba, the capital city of South Sudan. AWOESO was formed in 2000, as a community based
+                        organization to respond to the overwhelming and increasing number of orphans, widows,
+                        internally displaced persons (IDPs), in the Equatorial Region (Central, Eastern and Western
+                        States) and South Sudan at large
                     </div>
                     <div class="quote-box">
-                        <div class="quote-text">Our vision is “To serve the humanity with our exceptional programs. We offer greater independence, more sustainability & superior donation methods.</div>
+                        <div class="quote-text">Our Mission is “To serve the most vulnerable people in south Sudan.</div>
                         <div class="quote-info">
-                            <div class="author-thumb"><img src="{{ Vite::asset('resources/images/resource/author-thumb-1.jpg') }}" alt=""></div>
-                            <div class="author-title">Sandy Kates</div>
-                            <div class="signature">Sandy Kates</div>
+                            <div class="author-thumb"><img
+                                    src="{{ Vite::asset('resources/images/resource/author-ceo.jpg') }}"
+                                    alt=""></div>
+                            <div class="author-title">Amule Samuel Williams</div>
+                            <div class="signature">Amule Samuel Williams</div>
                         </div>
                     </div>
                 </div>
@@ -32,11 +33,12 @@
             <!--Image Column-->
             <div class="image-column col-lg-5 col-md-12 col-sm-12">
                 <div class="inner wow fadeInLeft">
-                    <figure class="image-box"><img src="{{ Vite::asset('resources/images/resource/featured-image-4.jpg') }}" alt=""></figure>
+                    <figure class="image-box"><img
+                            src="{{ Vite::asset('resources/images/resource/featured-image-4.jpg') }}" alt="">
+                    </figure>
                 </div>
             </div>
         </div>
     </div>
 
 </section>
-
--- resources/views/layouts/partials/motto.blade.php
+++ resources/views/layouts/partials/motto.blade.php
@@ -1,39 +1,51 @@
  <!--Moto Section-->
  <section class="our-moto">
-    <div class="auto-container">
-        <div class="inner-container">
-            <div class="row clearfix">
-                <!--Moto Block-->
-                <div class="moto-block col-lg-4 col-md-6 col-sm-12">
-                    <div class="inner-box">
-                        <div class="inner">
-                            <div class="icon"><span class="flaticon-care"></span></div>
-                            <h3>Helping Mankind</h3>
-                            <div class="text">Auis nostrud exercitation ullam labor aliquip exsed consequat duis autpsum dolore magna aliqua minim</div>
-                        </div>
-                    </div>
-                </div>
-                <!--Moto Block-->
-                <div class="moto-block col-lg-4 col-md-6 col-sm-12">
-                    <div class="inner-box">
-                        <div class="inner">
-                            <div class="icon"><span class="flaticon-pigeon"></span></div>
-                            <h3>Love Your World</h3>
-                            <div class="text">Auis nostrud exercitation ullam labor aliquip exsed consequat duis autpsum dolore magna aliqua minim</div>
-                        </div>
-                    </div>
-                </div>
-                <!--Moto Block-->
-                <div class="moto-block col-lg-4 col-md-6 col-sm-12">
-                    <div class="inner-box">
-                        <div class="inner">
-                            <div class="icon"><span class="flaticon-harvest"></span></div>
-                            <h3>Food for Everyone</h3>
-                            <div class="text">Auis nostrud exercitation ullam labor aliquip exsed consequat duis autpsum dolore magna aliqua minim</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
+     <div class="auto-container">
+         <div class="inner-container">
+             <div class="row clearfix">
+                 <!--Moto Block-->
+                 <div class="moto-block col-lg-4 col-md-6 col-sm-12">
+                     <div class="inner-box">
+                         <div class="inner">
+                             <div class="icon"><span class="flaticon-care"></span></div>
+                             <h3>Health</h3>
+                             <div class="text">
+                                 In order to break the cycle of malnutrition and hunger, AWOESO ensures
+                                 that expecting and new mothers and their children have health and nutritional support
+                                 that they require.
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+                 <!--Moto Block-->
+                 <div class="moto-block col-lg-4 col-md-6 col-sm-12">
+                     <div class="inner-box">
+                         <div class="inner">
+                             <div class="icon"><span class="flaticon-pigeon"></span></div>
+                             <h3>Women Emanicipation</h3>
+                             <div class="text">
+                                 AWOESO trains women in different life skills for example, tailoring, fashion and
+                                 design, agriculture, financial literacy. All these provide the women with skills which
+                                 they use to earn income that they use to improve on their standards of living
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+                 <!--Moto Block-->
+                 <div class="moto-block col-lg-4 col-md-6 col-sm-12">
+                     <div class="inner-box">
+                         <div class="inner">
+                             <div class="icon"><span class="flaticon-harvest"></span></div>
+                             <h3>Humanitarian Relief</h3>
+                             <div class="text">AWOESO provides relief/Assistance in form of food,
+                                 clothes, shelter, emergency medical relief, Mattresses to the most vulnerable people
+                                 (Internally Displaced persons, widows, children etc)
+
+                             </div>
+                         </div>
+                     </div>
+                 </div>
+             </div>
+         </div>
+     </div>
+ </section>
